d1108678a8ebf0c6d0513cd6653c1fdd497d9b53
Jared Rummler <jaredrummler@gmail.com>
2019-01-16 11:02:12 -0800

    Toggle full screen binding

--- init.lua
+++ init.lua
@@ -1,10 +1,19 @@
+super = { "cmd", "alt", "ctrl" }
+
 require "microwindow"
 require "winman"
 require "shiftit"
 require "applaunch"
 require "mocha"
 
+-- Toggle full screen
+hs.hotkey.bind(super, "t", function()
+    if hs.window.focusedWindow() then
+        hs.window.frontmostWindow():toggleZoom()
+    end
+end)
+
 -- reloading
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
+hs.hotkey.bind(super, "r", function()
     hs.reload()
 end)
